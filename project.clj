a40fd5ce0efcc59db7370a527ab3eb1a1f9595a3
Nicholas Volk <nicholas.volk@rtcritical.com>
2021-04-06 08:56:52 -0600

    prepend name with rtcritical

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject rtengine-service-connection "1.0.0"
+(defproject rtcritical/rtengine-service-connection "1.0.0"
   :description "Service Connection for RTEngine"
   :url "https://github.com/RTCritical/RTEngine Service Connection"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url  "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies []
   :plugins []
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}})
